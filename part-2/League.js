0ce53d50c9544a77f1a04978125c0ba5791550fb
VitaLiNaLi <vitailina5@gmail.com>
2023-11-07 17:55:42 +0300

    Part 2

--- part-2/League.js
+++ part-2/League.js
@@ -1,9 +1,31 @@
-const { PointsCalculator } = require('./PointsCalculator');
+const { PointsCalculator } = require("./PointsCalculator");
+const fs = require("fs").promises;
 
 class League {
   constructor(options = {}) {
     this.teams = options.teams || [];
     this.calculator = options.calculator || new PointsCalculator();
+  }
+
+  standings() {
+      .slice(0)
+      .sort(
+        (a, b) =>
+          b.wins * 2 + b.overtimeLosses - (a.wins * 2 + a.overtimeLosses)
+      );
+
+    return sortable;
+  }
+
+  async writeStandings(file) {
+    const meta=this.standings().reduce((a, b) => a + `${this.standings().indexOf(b)+1}. ${b.name} - ${b.wins * 2 + b.overtimeLosses} points\n`, "")
+    await fs.writeFile(file, meta);
+  }
+
+  scoresOf(name) {
+    const all = this.standings().filter((el) => el.name == name);
+
+    return all[0].wins * 2 + all[0].overtimeLosses;
   }
 }
 
